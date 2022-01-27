375834ef34fb03e37a72f246102a3ca94358f547
ilya <iliagr2007@gmail.com>
2022-01-27 15:20:52 +0300

    final version

--- final.py
+++ final.py
@@ -7,22 +7,36 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 450
 tile_width = tile_height = 50
 
+active_sprite_list = pygame.sprite.Group()
+platform_list = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+castle_list = pygame.sprite.Group()
+coins_list = pygame.sprite.Group()
+floor_list = pygame.sprite.Group()
+
 pygame.init()
 size = [SCREEN_WIDTH, SCREEN_HEIGHT]
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Платформер")
 
-active_sprite_list = pygame.sprite.Group()
-platform_list = pygame.sprite.Group()
-all_sprites = pygame.sprite.Group()
+coin_sound = pygame.mixer.Sound("data/claim_coin_mus.mp3")
+start_music = pygame.mixer.Sound("data/start_screen_mus.mp3")
+final_music = pygame.mixer.Sound("data/gameover_mus.mp3")
+win_music = pygame.mixer.Sound("data/level_clear_mus.mp3")
 
+maps_list, level_list = [open('data/map_1.txt').readlines()], ['map_1.txt', 'map_2.txt']
+level_musics = ["data/level_1_mus.mp3", "data/level_1_mus.mp3"]
+Level = len(level_list)
+World = 1
 live = 3
+coins = 0
 
-maps_list, level_list = [open('data/map_1.txt').readlines(), open('data/map_2.txt').readlines()],\
-                        ['map_1.txt', 'map_2.txt']
+score1 = []
 
 clock = pygame.time.Clock()
 FPS = 60
+castle_x = 0
+Win = False
 end = False
 
 
@@ -58,11 +72,11 @@ def load_level(filename):
 
 tile_images = {'platform': 'block.png', 'box': 'box.png', 'lt_up_pipe': 'lt_up_pipe.png',
                'floor': 'floor.png', 'lt_dn_pipe': 'lt_dn_pipe.png', 'rt_up_pipe': 'rt_up_pipe.png',
-               'rt_dn_pipe': 'rt_dn_pipe.png', 'castle': 'castle.png'}
+               'rt_dn_pipe': 'rt_dn_pipe.png', 'castle': 'castle.png', 'coin': 'coin.png'}
 
 
 class Player(pygame.sprite.Sprite):
-    global platform_list
+    global platform_list, coins
     right = True
 
     def __init__(self, x, y):
@@ -76,6 +90,8 @@ class Player(pygame.sprite.Sprite):
 
         self.stay = [("data/mario0.png", 1)]
         self.block_hit_list = []
+        self.castle_hit_list = []
+        self.coins_hit_list = []
 
         self.imagesr = [(load_image("mario1.png"), 100), (load_image("mario2.png"), 100),
                         (load_image("mario3.png"), 100), (load_image("mario2.png"), 100)]
@@ -113,6 +129,7 @@ class Player(pygame.sprite.Sprite):
         self.change_y = 0
 
     def update(self):
+        global coins
         self.calc_grav()
 
         if self.move == 'stay':
@@ -139,27 +156,40 @@ class Player(pygame.sprite.Sprite):
         if (SCREEN_WIDTH / 2 > player.total_x) or (player.total_x > (LEVEL_WIDTH - SCREEN_WIDTH / 2)):
             self.rect.x += self.change_x
 
-        self.total_x += self.change_x
         self.block_hit_list = pygame.sprite.spritecollide(self, platform_list, False)
 
         for block in self.block_hit_list:
-            if self.change_x > 0:
-                self.rect.right = block.rect.left
+            if block in coins_list:
+                block.kill()
+                coin_sound.set_volume(0.2)
+                coin_sound.play(0)
+                coins += 1
+            else:
+                if self.change_x > 0:
+                    self.rect.right = block.rect.left
 
-            elif self.change_x < 0:
-                self.rect.left = block.rect.right
+                elif self.change_x < 0:
+                    self.rect.left = block.rect.right
+                self.change_x = 0
+                self.stop()
 
         self.rect.y += self.change_y
         self.block_hit_list = pygame.sprite.spritecollide(self, platform_list, False)
 
         for block in self.block_hit_list:
-            if self.change_y > 0:
-                self.rect.bottom = block.rect.top
+            if block in coins_list:
+                block.kill()
+                coins += 1
+            else:
+                if self.change_y > 0:
+                    self.rect.bottom = block.rect.top
 
-            elif self.change_y < 0:
-                self.rect.top = block.rect.bottom
+                elif self.change_y < 0:
+                    self.rect.top = block.rect.bottom
 
             self.change_y = 0
+
+        self.total_x += self.change_x
 
     def jump(self):
         self.rect.y += 10
@@ -191,9 +221,11 @@ class Player(pygame.sprite.Sprite):
         surf.blit(self.image, (self.total_x, self.total_y))
 
     def die(self):
+        global coins
         pygame.mixer.music.load("data/gameover_mus.mp3")
         pygame.mixer.music.play(loops=0, start=0.0, fade_ms=0)
-        lives(True)
+        counters.lives(True)
+        coins = 0
         if live == 0:
             final_screen()
         else:
@@ -204,6 +236,18 @@ class Tile(pygame.sprite.Sprite):
     def __init__(self, tile_type, pos_x, pos_y):
         super().__init__(all_sprites, platform_list)
         self.image = load_image(tile_images[tile_type])
+
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
+class UsableTile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y, group):
+        super().__init__(all_sprites, platform_list, group)
+        self.image = load_image(tile_images[tile_type])
+        global castle_x
+        if group == castle_list:
+            castle_x = pos_x * 50
 
         self.rect = self.image.get_rect().move(
             tile_width * pos_x, tile_height * pos_y)
@@ -237,10 +281,13 @@ def draw(level):
                 Tile('rt_dn_pipe', x, y)
 
             elif level[y][x] == 'f':
-                Tile('floor', x, y)
+                UsableTile('floor', x, y, floor_list)
 
             elif level[y][x] == 'C':
-                Tile('castle', x, y)
+                UsableTile('castle', x, y, castle_list)
+
+            elif level[y][x] == '0':
+                UsableTile('coin', x, y, coins_list)
 
     return new_player, x, y
 
@@ -262,22 +309,38 @@ def terminate():
     sys.exit()
 
 
-def timer():
-    font = pygame.font.SysFont('comicsansms', 26)
-    time1 = int(pygame.time.get_ticks() / 1000) - 4
-    text = 'Time:' + str(time1)
-    time = font.render(text, True, (255, 255, 255))
-    return [time, time1]
+class Counters:
+    def __init__(self):
+        pass
 
+    def timer(self):
+        font = pygame.font.SysFont('comicsansms', 26)
+        time1 = int(pygame.time.get_ticks() / 1000) - 4
+        text = 'Time:' + str(time1)
+        time = font.render(text, True, (255, 255, 255))
+        return [time, time1]
 
-def lives(minus):
-    global live
-    if minus:
-        live -= 1
-    font = pygame.font.SysFont('comicsansms', 26)
-    text = "Осталось жизней - " + str(live)
-    time = font.render(text, True, (255, 255, 255))
-    return time
+    def coin(self):
+        font = pygame.font.SysFont('comicsansms', 26)
+        text = 'Coins x' + str(int(player.coins))
+        coin = font.render(text, True, (255, 255, 255))
+        return coin
+
+    def score(self):
+        font = pygame.font.SysFont('comicsansms', 26)
+        score1 = int(coins)
+        text = 'Score:' + str(score1)
+        score = font.render(text, True, (255, 255, 255))
+        return [score, score1]
+
+    def lives(self, minus):
+        global live
+        if minus:
+            live -= 1
+        font = pygame.font.SysFont('comicsansms', 26)
+        text = "Осталось жизней - " + str(live)
+        time = font.render(text, True, (255, 255, 255))
+        return time
 
 
 def start_screen():
@@ -318,7 +381,7 @@ def final_screen():
                 sys.exit()
         if k == 0:
             font = pygame.font.SysFont('comicsansms', 26)
-            text = 'Твоё время - ' + str(timer()[1])
+            text = 'Твоё время - ' + str(counters.timer()[1])
             time = font.render(text, True, (255, 255, 255))
             screen.blit(time, (0, 0))
             k += 1
@@ -333,6 +396,7 @@ def win(lvl):
 
     for sprite in active_sprite_list:
         sprite.kill()
+
     for sprite in platform_list:
         sprite.kill()
 
@@ -358,6 +422,21 @@ def win(lvl):
         text = 'Вы победили!!!'
         lvl_up = font.render(text, True, (255, 255, 255))
         screen.blit(lvl_up, (300, 225))
+
+        screen.blit(counters.timer()[0], (200, 0))
+        screen.blit(counters.score()[0], (550, 0))
+
+        try:
+            f = open("best_score.txt")
+            for i in f.readlines():
+                if int(i.split()[0]) > int(counters.timer()[1]) and int(i.split()[2]) <= counters.score()[1]:
+                    open('best_score.txt', 'w').close()
+                    f = open("best_score.txt")
+                    f.write(f'{counters.timer()[1]} - {counters.score()[1]}')
+
+        except IOError:
+            print("No file")
+
         pygame.display.flip()
 
         while True:
@@ -371,7 +450,7 @@ def win(lvl):
 
 
 def game(lvl):
-    global player, LEVEL_WIDTH, LEVEL_HEIGHT
+    global player, LEVEL_WIDTH, LEVEL_HEIGHT, counters
 
     for sprite in active_sprite_list:
         sprite.kill()
@@ -384,6 +463,7 @@ def game(lvl):
 
     running = True
     camera = Camera()
+    counters = Counters()
 
     active_sprite_list.add(player)
 
@@ -436,10 +516,12 @@ def game(lvl):
         if player.rect.bottom == 500:
             player.die()
 
-        if player.total_x >= 3916 and lvl == 0:
-            win(0)
-        elif player.total_x >= 5700 and lvl == 1:
-            win(1)
+        if player.total_x >= castle_x - 50:
+            if lvl == 0:
+                win(0)
+            else:
+                win(1)
+            running = False
 
         if lvl == 0:
             screen.blit(fon, (0, 0))
@@ -449,8 +531,9 @@ def game(lvl):
         platform_list.draw(screen)
         active_sprite_list.draw(screen)
 
-        screen.blit(timer()[0], (400, 0))
-        screen.blit(lives(False), (0, 0))
+        screen.blit(counters.timer()[0], (400, 0))
+        screen.blit(counters.lives(False), (0, 0))
+        screen.blit(counters.score()[0], (600, 0))
 
         pygame.display.flip()
         clock.tick(FPS)
@@ -458,4 +541,4 @@ def game(lvl):
     pygame.quit()
 
 
-game(1)
+game(0)
